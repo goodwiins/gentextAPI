e6c1ba905ed33e78213091313b8707d20ff420cd
abdelghafour el bikha <allocs16@gmail.com>
2024-03-07 14:43:32 -0500

    Add proxy configuration and fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [data, setdata] = useState({
+    text:"",
+    response: ""
+});
+useEffect(() => {
+  // Using fetch to fetch the api from
+  // flask server it will be redirected to proxy
+  fetch("/data").then((res) =>
+      res.json().then((data) => {
+          // Setting a data from api
+          setdata({
+              text: data.text,
+              response: data.response
+          });
+      })
   );
+}, []);
+return (
+  <div className="App">
+      <header className="App-header">
+          <h1>React and flask</h1>
+          {/* Calling a data from setdata for showing */}
+          <p>{data.text}</p>
+          <p>{data.response}</p>
+         
+
+      </header>
+  </div>
+);
+
 }
 
 export default App;
